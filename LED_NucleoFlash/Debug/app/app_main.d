839e229620740ba0b739442dac6289ac256247f5
Tinytinfoil <55459863+TinyTinfoil@users.noreply.github.com>
2023-07-14 20:38:54 -0400

    Add CMSIS DSP v1.14.4

--- LED_NucleoFlash/Debug/app/app_main.d
+++ LED_NucleoFlash/Debug/app/app_main.d
@@ -26,7 +26,39 @@ app/app_main.o: ../app/app_main.c ../Core/Inc/main.h \
  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h \
  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim.h \
- ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h \
+ ../Drivers/CMSIS/DSP/Include/arm_math.h \
+ ../Drivers/CMSIS/DSP/Include/arm_math_types.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/DSP/Include/arm_math_memory.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/none.h \
+ ../Drivers/CMSIS/DSP/Include/arm_math_types.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/utils.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/basic_math_functions.h \
+ ../Drivers/CMSIS/DSP/Include/arm_math_memory.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/none.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/utils.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/interpolation_functions.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/bayes_functions.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/statistics_functions.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/basic_math_functions.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/fast_math_functions.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/matrix_functions.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/complex_math_functions.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/statistics_functions.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/controller_functions.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/support_functions.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/distance_functions.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/matrix_functions.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/svm_functions.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/svm_defines.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/fast_math_functions.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/transform_functions.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/complex_math_functions.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/filtering_functions.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/support_functions.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/quaternion_math_functions.h \
+ ../Drivers/CMSIS/DSP/Include/dsp/window_functions.h
 ../Core/Inc/main.h:
 ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
 ../Core/Inc/stm32l4xx_hal_conf.h:
@@ -56,3 +88,35 @@ app/app_main.o: ../app/app_main.c ../Core/Inc/main.h \
 ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h:
 ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim.h:
 ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h:
+../Drivers/CMSIS/DSP/Include/arm_math.h:
+../Drivers/CMSIS/DSP/Include/arm_math_types.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/DSP/Include/arm_math_memory.h:
+../Drivers/CMSIS/DSP/Include/dsp/none.h:
+../Drivers/CMSIS/DSP/Include/arm_math_types.h:
+../Drivers/CMSIS/DSP/Include/dsp/utils.h:
+../Drivers/CMSIS/DSP/Include/dsp/basic_math_functions.h:
+../Drivers/CMSIS/DSP/Include/arm_math_memory.h:
+../Drivers/CMSIS/DSP/Include/dsp/none.h:
+../Drivers/CMSIS/DSP/Include/dsp/utils.h:
+../Drivers/CMSIS/DSP/Include/dsp/interpolation_functions.h:
+../Drivers/CMSIS/DSP/Include/dsp/bayes_functions.h:
+../Drivers/CMSIS/DSP/Include/dsp/statistics_functions.h:
+../Drivers/CMSIS/DSP/Include/dsp/basic_math_functions.h:
+../Drivers/CMSIS/DSP/Include/dsp/fast_math_functions.h:
+../Drivers/CMSIS/DSP/Include/dsp/matrix_functions.h:
+../Drivers/CMSIS/DSP/Include/dsp/complex_math_functions.h:
+../Drivers/CMSIS/DSP/Include/dsp/statistics_functions.h:
+../Drivers/CMSIS/DSP/Include/dsp/controller_functions.h:
+../Drivers/CMSIS/DSP/Include/dsp/support_functions.h:
+../Drivers/CMSIS/DSP/Include/dsp/distance_functions.h:
+../Drivers/CMSIS/DSP/Include/dsp/matrix_functions.h:
+../Drivers/CMSIS/DSP/Include/dsp/svm_functions.h:
+../Drivers/CMSIS/DSP/Include/dsp/svm_defines.h:
+../Drivers/CMSIS/DSP/Include/dsp/fast_math_functions.h:
+../Drivers/CMSIS/DSP/Include/dsp/transform_functions.h:
+../Drivers/CMSIS/DSP/Include/dsp/complex_math_functions.h:
+../Drivers/CMSIS/DSP/Include/dsp/filtering_functions.h:
+../Drivers/CMSIS/DSP/Include/dsp/support_functions.h:
+../Drivers/CMSIS/DSP/Include/dsp/quaternion_math_functions.h:
+../Drivers/CMSIS/DSP/Include/dsp/window_functions.h:
--- LED_NucleoFlash/Debug/app/subdir.mk
+++ LED_NucleoFlash/Debug/app/subdir.mk
@@ -16,7 +16,7 @@ OBJS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 app/%.o app/%.su app/%.cyclo: ../app/%.c app/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32L432xx -c -I../Core/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32L4xx/Include -I../Drivers/CMSIS/Include -I"/home/venkadesh/Desktop/ADE Global Health/oae_firmware/LED_NucleoFlash/app" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32L432xx -c -I../Core/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32L4xx/Include -I../Drivers/CMSIS/Include -I"C:/Users/veswaranandam/OneDrive - Olin College of Engineering/Desktop/ADE Global Health/oae_firmware/LED_NucleoFlash/app" -I../Drivers/CMSIS/DSP/Include -I../Drivers/CMSIS/DSP/PrivateInclude -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-app
 
