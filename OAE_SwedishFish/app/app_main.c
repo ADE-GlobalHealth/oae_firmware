0582b76f9667edad22d79a7a92fa8fb5dac15c06
KLA-pie <klieatjam@olin.edu>
2023-07-27 16:51:29 -0400

    Organized ADC FFT structure - Configured STM and DMA pinouts - Reconfigured I2S clock - Initialized app_main.c - Restructured CMSIS DSP Library - Removed LED_NucleoFlash

---
+++ OAE_SwedishFish/app/app_main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "arm_math.h"
+
+//Runs once after all peripherals are initialized
+void app_setup(){
+
+}
+
+//Runs repeatedly after app_setup() has finished
+void app_loop() {
+
+}
+
