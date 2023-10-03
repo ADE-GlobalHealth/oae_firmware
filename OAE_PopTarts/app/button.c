d495a55c9dd5e116a02eece69d21ef2f93cad0b3
Tinytinfoil <55459863+TinyTinfoil@users.noreply.github.com>
2023-10-03 16:49:02 -0400

    Perform file split of app_main.c

--- OAE_PopTarts/app/button.c
+++ OAE_PopTarts/app/button.c
@@ -1,32 +1,32 @@
-// #include "button.h"
+#include "button.h"
 
-// #define CHANGE_INTERVAL 50
-// bool CheckButtonState(GPIO_TypeDef* port,GPIO_TypeDef* pin, unsigned long time)
-// {
-//   static unsigned long lastDebounce = 0;
-//   // Check if its appropriate to test the button state
-//   if ((time - lastDebounce) >= CHANGE_INTERVAL)
-//   {
-//     // Do debounce
-//     if (debounce(port,pin))
-//     {
-//       return true;
-//     }
-//     // Update the last debounce check time
-//     lastDebounce = HAL_GetTick();
-//   }
-//   return false;
-// }
+#define CHANGE_INTERVAL 50
+bool CheckButtonState(GPIO_TypeDef* port,GPIO_TypeDef* pin, unsigned long time)
+{
+  static unsigned long lastDebounce = 0;
+  // Check if its appropriate to test the button state
+  if ((time - lastDebounce) >= CHANGE_INTERVAL)
+  {
+    // Do debounce
+    if (debounce(port,pin))
+    {
+      return true;
+    }
+    // Update the last debounce check time
+    lastDebounce = HAL_GetTick();
+  }
+  return false;
+}
 
-// //Stores history of button states
-// uint16_t btnStates = 0;
+//Stores history of button states
+uint16_t btnStates = 0;
 
-// bool debounce(GPIO_TypeDef* port,GPIO_TypeDef* pin)
-// {
-//   /* based off of http://www.ganssle.com/debouncing-pt2.htm
-//   - stores the button press history as a sequence of bits
-//   - if the sequence matches a rising edge (0000 0000 1111 1111), a valid button press is detected
-//   */
-//   btnStates = (btnStates << 1) | (!HAL_GPIO_ReadPin(SW1_GPIO_Port,SW1_Pin));
-//   return (btnStates == 0x00FF);
-// }
+bool debounce(GPIO_TypeDef* port,GPIO_TypeDef* pin)
+{
+  /* based off of http://www.ganssle.com/debouncing-pt2.htm
+  - stores the button press history as a sequence of bits
+  - if the sequence matches a rising edge (0000 0000 1111 1111), a valid button press is detected
+  */
+  btnStates = (btnStates << 1) | (!HAL_GPIO_ReadPin(SW1_GPIO_Port,SW1_Pin));
+  return (btnStates == 0x00FF);
+}
--- OAE_PopTarts/app/button.h
+++ OAE_PopTarts/app/button.h
@@ -1,10 +1,12 @@
-// #ifndef BUTTON_H_
-// #define BUTTON_H_
+#ifndef BUTTON_H_
+#define BUTTON_H_
 
-// #include <stm32l4xx_hal.h>
+#include <stm32l4xx_hal.h>
+#include <stdbool.h>
+#include <main.h>
 
-// bool CheckButtonState(GPIO_TypeDef* port,GPIO_TypeDef* pin, unsigned long time);
-// bool debounce(GPIO_TypeDef* port,GPIO_TypeDef* pin);
+bool CheckButtonState(GPIO_TypeDef* port,GPIO_TypeDef* pin, unsigned long time);
+bool debounce(GPIO_TypeDef* port,GPIO_TypeDef* pin);
 
 
-// #endif /* BUTTON_H_ */
+#endif /* BUTTON_H_ */
