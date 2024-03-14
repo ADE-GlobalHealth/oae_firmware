1539aec335d8dd7eb3298cd63acde5529f859c35
ayushchakra <achakraborty@olin.edu>
2024-03-14 16:38:19 -0400

    working dma

--- OAE_PopTarts/Core/Src/main.c
+++ OAE_PopTarts/Core/Src/main.c
@@ -291,7 +291,7 @@ static void MX_SAI2_Init(void)
   hsai_BlockA2.Init.FIFOThreshold = SAI_FIFOTHRESHOLD_EMPTY;
   hsai_BlockA2.Init.AudioFrequency = SAI_AUDIO_FREQUENCY_48K;
   hsai_BlockA2.Init.SynchroExt = SAI_SYNCEXT_DISABLE;
-  hsai_BlockA2.Init.MonoStereoMode = SAI_STEREOMODE;
+  hsai_BlockA2.Init.MonoStereoMode = SAI_MONOMODE;
   hsai_BlockA2.Init.CompandingMode = SAI_NOCOMPANDING;
   if (HAL_SAI_InitProtocol(&hsai_BlockA2, SAI_I2S_STANDARD, SAI_PROTOCOL_DATASIZE_16BIT, 2) != HAL_OK)
   {
