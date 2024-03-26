d1f404bb8db543f2ff4c9b63aad1a9b1aa32f4be
Benji Pugh <pughbenji@gmail.com>
2024-03-26 17:38:37 -0400

    Add pingpong buffer copying routines

--- OAE_PopTarts/app/oae_algorithm.c
+++ OAE_PopTarts/app/oae_algorithm.c
@@ -5,6 +5,10 @@
 #define NUM_NF_VALS 3*2 // Has to be a multiple of 2
 #define NF_MAXIMUM 10000.0 // Placeholder Value. TODO: Update based on histograms of actual values and sensitivity.
 
+typedef enum {
+    HALF_TRANSFER,
+    FULL_TRANSFER
+    } DMA_status;
 
 void oae_algorithm(float32_t* accumulator, uint32_t* num_successful, const int32_t* sample_buffer, arm_rfft_fast_instance_f32* rfft, float32_t* window_lut) {
     static float32_t converted_buffer[FFT_BUFFER_SIZE];
@@ -50,5 +54,42 @@ void ADCbuff_to_float32(int32_t* pSrc, float32_t* pDst, uint32_t numSamples) {
         pDst[i] = pSrc[i] >> 8;
     }
 
+}
 
+
+void switch_pingpong_buffer(const int32_t* bufA, const int32_t* bufB, int32_t ** DMA_addr, bool isA){
+    // Switch the pingpong buffer
+    if(isA){
+        DMA_addr = bufB;
+    } else {
+        DMA_addr = bufA;
+    }
+    isA = ~isA;
+}
+
+void get_sample_buffer(int* sample_buffer, const int32_t* bufA, const int32_t* bufB, const bool isA, DMA_status ADC_status){
+    // Expects that bufA and bufB are the length of a full fft buffer (FFT_BUFFER_SIZE)
+    // Sample buffer is the output.
+    if (isA){
+        if (ADC_status == HALF_TRANSFER) {
+            memcpy(sample_buffer,  bufB+FFT_BUFFER_SIZE/2, FFT_BUFFER_SIZE/2);
+            memcpy(sample_buffer+FFT_BUFFER_SIZE/2,  bufA, FFT_BUFFER_SIZE/2);
+        }
+        else {
+            memcpy(sample_buffer,  bufA, FFT_BUFFER_SIZE/2);
+        }
+    }else{
+        if (ADC_status == HALF_TRANSFER) {
+            memcpy(sample_buffer,  bufA+FFT_BUFFER_SIZE/2, FFT_BUFFER_SIZE/2);
+            memcpy(sample_buffer+FFT_BUFFER_SIZE/2,  bufB, FFT_BUFFER_SIZE/2);
+        }
+        else {
+            memcpy(sample_buffer,  bufB, FFT_BUFFER_SIZE/2);
+        }
+    }
+}
+
+void ADC_interrupt_routine(){
+    // Handles the ADC DMA full and half transfer routine
+    
 }
