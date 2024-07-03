ff567bc95d17e62812ae0b56be9ede75da0f900f
Benji Pugh <pughbenji@gmail.com>
2024-07-03 12:03:16 -0400

    Merge OAE Algorithm (#5)
    * Finish first pass of OAE algorithm
    Created signal processing algorithm that will be run repeatedly.
    Will need memory optimization.
    
    * Add pingpong buffer copying routines
    
    * Changed functions to use wrapper structures.
    
    * Added setup function for oae_data_t
    
    * Changed buffers to share the same memory.
    
    * Added header file and switched the algorithm to use dB

---
+++ OAE_PopTarts/app/oae_algorithm.c
@@ -0,0 +1,116 @@
+/*
+ * oae_algorithm.c
+ *
+ *  Created on: February, 2024
+ *      Author: benjipugh
+ */
+
+
+#include "oae_algorithm.h"
+
+oae_data_t* setup_oae_data(){
+    oae_data_t *oae_data_ptr = (oae_data_t*)malloc(sizeof(oae_data_t));
+
+    #if FFT_BUFFER_SIZE != 2048
+        #error Currently is only able to deal with 2048 sized fft buffer
+    #endif
+    arm_cfft_init_2048_f32(&(oae_data_ptr->fft));
+    oae_data_ptr->num_sub_nf_threshold = 0;
+    oae_data_ptr->oae_accumulator = 0;
+    arm_hamming_f32(&(oae_data_ptr->window_lut), FFT_BUFFER_SIZE);
+    return oae_data_ptr;
+}
+
+void oae_algorithm(oae_data_t *oae_data, const int32_t* sample_buffer) {
+    static float32_t converted_buffer[FFT_BUFFER_SIZE];
+    float32_t * windowed_buffer = converted_buffer;
+
+    static float32_t fft_output[FFT_BUFFER_SIZE*2];
+    float32_t * fft_output_abs = converted_buffer;
+    float32_t * fft_output_dB = converted_buffer;
+    float32_t average_noise_floor;
+
+    ADCbuff_to_float32(sample_buffer, converted_buffer, FFT_BUFFER_SIZE);
+
+    arm_mult_f32(oae_data->window_lut, converted_buffer, windowed_buffer, FFT_BUFFER_SIZE);
+    
+    
+    arm_rfft_fast_f32(&(oae_data->fft), converted_buffer, fft_output, 0);
+    
+    // Convert to fft magnitudes
+    arm_cmplx_mag_f32(fft_output, fft_output_abs, FFT_BUFFER_SIZE);
+    // Convert to dB. This is sort of a worst case scenario since this is performing log on the entire set of samples
+    arm_vlog_f32(fft_output_abs, fft_output_dB, FFT_BUFFER_SIZE);
+    arm_scale_f32(fft_output_dB, SCALE_DB, fft_output_dB, FFT_BUFFER_SIZE);
+
+    // There will be a more elegant way to execute this
+    for (int i = 5; i<NUM_NF_VALS/2; i++) {
+        average_noise_floor = fft_output_dB[FFT_OAE_IDX + i];
+    }
+    for (int i = -NUM_NF_VALS/2-5; i< (-NUM_NF_VALS/2); i++) {
+        average_noise_floor = fft_output_dB[FFT_OAE_IDX + i];
+    }
+
+    if (average_noise_floor > NF_MAXIMUM) {
+        exit();
+    } else {
+        oae_data->oae_accumulator += fft_output_abs[FFT_OAE_IDX];
+        oae_data->nf_accumulator += average_noise_floor;
+        oae_data->num_sub_nf_threshold += 1;
+    }
+}
+
+float32_t convert_ADCout_to_float32(int32_t adc_value) {
+    // From: https://stackoverflow.com/questions/37468430/convert-24bit-twos-complement-to-float-32t
+    // I would like someone to doublecheck this.
+        return adc_value >> 8;
+}
+
+
+void ADCbuff_to_float32(int32_t* pSrc, float32_t* pDst, uint32_t numSamples) {
+    for (int i = 0; i < numSamples; i++){
+        pDst[i] = pSrc[i] >> 8;
+    }
+
+}
+
+//void interpolate_error_samples()
+
+void switch_pingpong_buffer(pingpong_buffers_t * bufs, int32_t ** DMA_addr){
+    // Switch the pingpong buffer
+    if(bufs->isA){
+        *DMA_addr = bufs->bufB;
+    } else {
+        *DMA_addr = bufs->bufA;
+    }
+    bufs->isA = !bufs->isA;
+}
+
+
+void ADC_half_transfer(pingpong_buffers_t * bufs){
+    // Expects that bufA and bufB are the length of a full fft buffer (FFT_BUFFER_SIZE)
+    // Sample buffer is the output.
+    if (bufs->isA)   {
+        memcpy(bufs->sample_buffer, bufs->bufB+FFT_BUFFER_SIZE/2, FFT_BUFFER_SIZE/2);
+        memcpy(bufs->sample_buffer+FFT_BUFFER_SIZE/2, bufs->bufA, FFT_BUFFER_SIZE/2);
+    } else {
+        memcpy(bufs->sample_buffer, bufs->bufA+FFT_BUFFER_SIZE/2, FFT_BUFFER_SIZE/2);
+        memcpy(bufs->sample_buffer+FFT_BUFFER_SIZE/2, bufs->bufB, FFT_BUFFER_SIZE/2);
+    }
+    
+}
+
+
+void ADC_full_transfer(pingpong_buffers_t * bufs, int32_t ** DMA_addr){
+    // Gets called when
+    // Expects that bufA and bufB are the length of a full fft buffer (FFT_BUFFER_SIZE)
+    // Sample buffer is the output.
+    if (bufs->isA) {
+        memcpy(bufs->sample_buffer,  bufs->bufA, FFT_BUFFER_SIZE/2);
+    }
+    else {
+        memcpy(bufs->sample_buffer,  bufs->bufB, FFT_BUFFER_SIZE/2);
+    }
+    switch_pingpong_buffer(bufs, DMA_addr);
+}
+
