8cbfe3aa5c32f9a1c515dfe5122b9e8aa7178555
Drew <silverbird337@gmail.com>
2025-05-07 19:56:16 -0400

    determine os in python setup script

--- scripts/setup_python_venv.sh
+++ scripts/setup_python_venv.sh
@@ -7,17 +7,38 @@ cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
 venv_name="oae_venv"
 
+# determine OS
+uname_out="$(uname -s)"
+case "${uname_out}" in
+Linux*) machine=Linux ;;
+CYGWIN* | MINGW* | MSYS*) machine=Windows ;;
+*) machine="UKNOWN:${uname_out}" ;;
+esac
+
 # check for existing venv and install python and depdencies via poetry if it
 # does not exist
 if [ ! -d ../${venv_name} ]; then
     pyenv install 3.11 --skip-existing
     pyenv local 3.11
-    python3 -m venv ${venv_name}
+
+    # install venv in project root
+    if [ "$machine" = "Windows" ]; then
+        python3 -m venv "../${venv_name}"
+    else
+        python3 -m venv "${venv_name}"
+    fi
     poetry install
 fi
 
-# shellcheck source=./oae_venv/bin/activate
-source ./${venv_name}/bin/activate
+# activate venv depending on OS
+if [ "$machine" = "Windows" ]; then
+    # shellcheck source=../oae_venv/bin/activate
+    source "../${venv_name}/Scripts/activate"
+else
+    # shellcheck source=../oae_venv/bin/activate
+    source "${venv_name}/bin/activate"
+fi
+
 # move python cache out of project directory
 export PYTHONPYCACHEPREFIX=/tmp/pycache
 export PYTHONPATH="$(pwd)"
