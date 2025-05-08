2bec5760af4eb2894d15c53d42f156e1df8fdfc2
Drew <silverbird337@gmail.com>
2025-05-08 18:17:11 -0400

    complete adc init

--- OAE_CliffBar/Drivers/tlv320adc3120/tlv320adc3120.c
+++ OAE_CliffBar/Drivers/tlv320adc3120/tlv320adc3120.c
@@ -9,48 +9,155 @@
 
 #include "tlv320adc3120.h"
 
-void _tlv_read_register(uint8_t address, uint8_t data) {
-	HAL_I2C_Mem_Read(&hi2c3, TLV_DEVICE_ID, address, 1, &data, 1,
+void _tlv_read_register(uint8_t address, uint8_t *data) {
+	HAL_I2C_Mem_Read(&hi2c3, TLV_DEVICE_ID, address, 1, data, 1,
 			HAL_MAX_DELAY);
 }
 
 
 void _tlv_write_register(uint8_t address, uint8_t data) {
-	HAL_I2C_Mem_Write(&hi2c3, TLV_DEVICE_ID, address, 1, &data, 1,
+	HAL_I2C_Mem_Write(&hi2c3, TLV_DEVICE_ID, address, 1, data, 1,
 			HAL_MAX_DELAY);
 }
 
 
-void _tlv_switch_register_page(uint8_t page_number) {
-	uint8_t switch_register_page_data = 0;
-	switch_register_page_data &= PAGE_CFG_PAGE_MASK;
-	switch_register_page_data |= page_number;
-	_tlv_write_register(PAGE_CFG_ADDRESS, switch_register_page_data);
+void _tlv_write_register_mask(uint8_t address, uint8_t mask, uint8_t data) {
+	// read data in register
+	uint8_t read_data;
+	_tlv_read_register(address, read_data);
+
+	// clear mask bits and or with write data
+	// NOTE: UB possible if mask does not cover the write data
+	read_data &= ~mask;
+	read_data |= data;
+	_tlv_write_register(address, read_data);
 }
 
 
-void tlv_init(void) {
-	;
+void _tlv_switch_register_page(uint8_t page_number) {
+	_tlv_write_register(PAGE_CFG_ADDRESS, page_number);
+}
+
+
+void tlv_init(tlv_config_t tlv_config) {
+	// perform a software reset to ensure TLV is at default
+	tlv_software_reset();
+
+	// AREG select
+	switch (tlv_config.areg_select) {
+	case INTERNAL:
+		_tlv_write_register_mask(SLEEP_CFG_ADDRESS, SLEEP_CFG_AREG_SELECT_MASK,
+		SLEEP_CFG_AREG_SELECT_INTERNAL);
+	case EXTERNAL:
+		_tlv_write_register_mask(SLEEP_CFG_ADDRESS, SLEEP_CFG_AREG_SELECT_MASK,
+		SLEEP_CFG_AREG_SELECT_EXTERNAL);
+	}
+
+	// ASI protocol
+	switch (tlv_config.asi_protocol) {
+	case TDM:
+		_tlv_write_register_mask(ASI_CFG0_ADDRESS, ASI_CFG0_FORMAT_MASK,
+		ASI_CFG0_FORMAT_TDM);
+	case I2S:
+		_tlv_write_register_mask(ASI_CFG0_ADDRESS, ASI_CFG0_FORMAT_MASK,
+		ASI_CFG0_FORMAT_I2S);
+	case LJ:
+		_tlv_write_register_mask(ASI_CFG0_ADDRESS, ASI_CFG0_FORMAT_MASK,
+		ASI_CFG0_FORMAT_LJ);
+	}
+
+	// ASI word length
+	switch (tlv_config.asi_word_length) {
+	case LENGTH_16b:
+		_tlv_write_register_mask(ASI_CFG0_ADDRESS, ASI_CFG0_WLEN_MASK,
+		ASI_CFG0_WLEN_16_BITS);
+	case LENGTH_20b:
+		_tlv_write_register_mask(ASI_CFG0_ADDRESS, ASI_CFG0_WLEN_MASK,
+		ASI_CFG0_WLEN_20_BITS);
+	case LENGTH_24b:
+		_tlv_write_register_mask(ASI_CFG0_ADDRESS, ASI_CFG0_WLEN_MASK,
+		ASI_CFG0_WLEN_24_BITS);
+	case LENGTH_32b:
+		_tlv_write_register_mask(ASI_CFG0_ADDRESS, ASI_CFG0_WLEN_MASK,
+		ASI_CFG0_WLEN_32_BITS);
+	}
+
+	// GPIO1 function
+	switch (tlv_config.gpio1_function) {
+	case DISABLED:
+		_tlv_write_register_mask(GPIO_CFG0_ADDRESS, GPIO_CFG0_GPIO1_CFG_MASK,
+				GPIO_CFG0_GPIO1_CFG_DISABLED);
+	case GPI:
+		_tlv_write_register_mask(GPIO_CFG0_ADDRESS, GPIO_CFG0_GPIO1_CFG_MASK,
+		GPIO_CFG0_GPIO1_CFG_GPI);
+	case GPO:
+		_tlv_write_register_mask(GPIO_CFG0_ADDRESS, GPIO_CFG0_GPIO1_CFG_MASK,
+		GPIO_CFG0_GPIO1_CFG_GPO);
+	case IRQ:
+		_tlv_write_register_mask(GPIO_CFG0_ADDRESS, GPIO_CFG0_GPIO1_CFG_MASK,
+		GPIO_CFG0_GPIO1_CFG_IRQ);
+	}
+
+	// analog input channel enable
+	uint8_t analog_in_data = IN_CH_EN_DEFAULT;
+	for (uint8_t i = 0; i < 4; i++) {
+		analog_in_data &= ~(0b1 << (8 - i));
+		analog_in_data |= tlv_config.in_ch_en[i] << (8 - i);
+	}
+	// mask not required, register is only used for analog channel
+	// enable/disable
+	_tlv_write_register(IN_CH_EN_ADDRESS, analog_in_data);
+
+	// digital output channel enable
+	uint8_t digital_out_data = ASI_OUT_CH_EN_DEFAULT;
+	for (uint8_t i = 0; i < 4; i++) {
+		digital_out_data &= ~(0b1 << (8 - i));
+		digital_out_data |= tlv_config.out_ch_en[i] << (8 - i);
+	}
+	// mask not required, register is only used for digital channel
+	// enable/disable
+	_tlv_write_register(ASI_OUT_CH_EN_ADDRESS, digital_out_data);
+
+	// micbias enable
+	switch (tlv_config.micbias_en) {
+	case ENABLED:
+		_tlv_write_register_mask(PWR_CFG_ADDRESS, PWR_CFG_MICBIAS_PDZ_MASK,
+		PWR_CFG_MICBIAS_PDZ_ON);
+	case DISABLED:
+		_tlv_write_register_mask(PWR_CFG_ADDRESS, PWR_CFG_MICBIAS_PDZ_MASK,
+		PWR_CFG_MICBIAS_PDZ_OFF);
+	}
+
+	// ADC enable
+	switch (tlv_config.adc_en) {
+	case ENABLED:
+		_tlv_write_register_mask(PWR_CFG_ADDRESS, PWR_CFG_ADC_PDZ_MASK,
+		PWR_CFG_ADC_PDZ_ON);
+	case DISABLED:
+		_tlv_write_register_mask(PWR_CFG_ADDRESS, PWR_CFG_ADC_PDZ_MASK,
+		PWR_CFG_ADC_PDZ_OFF);
+	}
+
 }
 
 
 void tlv_start_adc_conversions(void) {
-	// perform a software reset to ensure tlv is at default
-	tlv_software_reset();
+	;
 }
 
 
 void tlv_software_reset(void) {
-	uint8_t sw_reset_data = 0;
-	sw_reset_data &= ~SW_RESET_MASK;
-	sw_reset_data |= SW_RESET_RESET;
-	_tlv_write_register(SW_RESET_ADDRESS, sw_reset_data);
+	// rest of register is 0 by default, no masking required
+	_tlv_write_register(SW_RESET_ADDRESS, SW_RESET_RESET);
 }
 
 
-void tlv_sleep(uint8_t enabled) {
-	uint8_t sleep_data = 0;
-	sleep_data &= ~SLEEP_CFG_SLEEP_ENZ_MASK;
-	sleep_data |= enabled;
-	_tlv_write_register(sleep_data);
+void tlv_sleep(enable_e enabled) {
+	if (enabled == ENABLED) {
+		_tlv_write_register_mask(SLEEP_CFG_ADDRESS, SLEEP_CFG_SLEEP_ENZ_MASK,
+		SLEEP_CFG_SLEEP_ENZ_SLEEP);
+	} else {
+		_tlv_write_register_mask(SLEEP_CFG_ADDRESS, SLEEP_CFG_SLEEP_ENZ_MASK,
+		SLEEP_CFG_SLEEP_ENZ_ACTIVE);
+	}
 }
--- OAE_CliffBar/Drivers/tlv320adc3120/tlv320adc3120.h
+++ OAE_CliffBar/Drivers/tlv320adc3120/tlv320adc3120.h
@@ -63,7 +63,7 @@ typedef enum {
  * Audio serial interface word length enum.
  */
 typedef enum {
-	LENGTH_20b, LENGTH_24b, LENGTH_32b
+	LENGTH_16b, LENGTH_20b, LENGTH_24b, LENGTH_32b
 } asi_word_length_e;
 
 
@@ -96,7 +96,7 @@ typedef enum {
  * Read a register in the ADC.
  *
  * @param address (uint8_t) The register address to read.
- * @param data (uint8_t*) A pointer to store the data.
+ * @param data (uint8_t *) A pointer to store the data.
  */
 void _tlv_read_register(uint8_t address, uint8_t *data);
 
@@ -108,6 +108,18 @@ void _tlv_read_register(uint8_t address, uint8_t *data);
  * @param data (uint8_t) The data to write to the register.
  */
 void _tlv_write_register(uint8_t address, uint8_t data);
+
+/**
+ * Write a register in the ADC using a mask.
+ *
+ * Useful for only writing bits in the mask. Bits not contained in the mask will
+ * not be changed.
+ *
+ * @param address (uint8_t) The register address to write.
+ * @param mask (uint8_t) The mask to use for clearing and setting bits.
+ * @param data (uint8_t ) The data to write to the register.
+ */
+void _tlv_write_register_mask(uint8_t address, uint8_t mask, uint8_t data);
 
 
 /**
@@ -121,9 +133,9 @@ void _tlv_switch_register_page(uint8_t page_number);
 /**
  * Initialize the ADC with a device config.
  *
- * @param
+ * @param tlv_config (tlv_config_t) The config to initialize the TLV with.
  */
-void tlv_init(void);
+void tlv_init(tlv_config_t tlv_config);
 
 
 /**
@@ -142,5 +154,7 @@ void tlv_software_reset(void);
 
 /**
  * Put the TLV ADC to sleep.
+ *
+ * @ param enabled (enabled_e) If enabled, the TLV is put to sleep.
  */
-void tlv_sleep(void);
+void tlv_sleep(enable_e enabled);
