fc6adfbf1ea7ee859dea1bb5ddfa44c3972ee9b3
Drew <silverbird337@gmail.com>
2025-05-12 16:37:31 -0400

    cliffbar/dac: abstract dac to library
    dac functionality should not live in main, instead it should live in a
    library to reduce clutter in app_main

---
+++ OAE_CliffBar/app/oae_dac.h
@@ -0,0 +1,36 @@
+/**
+ * DAC functions for device use.
+ */
+
+#pragma once
+
+#include <stm32l4xx_hal.h>
+#include <stm32l4xx_hal_dac.h>
+
+#include "dual_dma.h"
+
+//extern DMA_HandleTypeDef hdma_dac_ch1; TODO: remove if not used
+extern TIM_HandleTypeDef htim6;
+extern DAC_HandleTypeDef hdac1;
+
+// TODO: differentiate between these
+#define DAC_LUT_SIZE 128
+#define DAC_LUT_SAMPLES 4096
+
+// DAC output look up table. Generating using tools/lut_gen.py.
+extern uint32_t dac_wave_lut[DAC_LUT_SAMPLES];
+
+/**
+ * Initialize timers used for DAC output.
+ */
+void init_dac(void);
+
+/**
+ * Start dual DMA output with the two DAC outputs.
+ */
+void start_dac_output(void);
+
+/**
+ * Stop dual DMA output of the two DAC outputs.
+ */
+void stop_dac_output(void);
